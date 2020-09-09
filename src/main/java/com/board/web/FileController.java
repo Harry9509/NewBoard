a5c2dd436d260c7bccab2d1f0e261c96667bd500
Harry <kumho9509@gmail.com>
2020-09-10 08:00:52 +0900

    2020.09.10-01

--- src/main/java/com/board/web/FileController.java
+++ src/main/java/com/board/web/FileController.java
@@ -22,5 +22,57 @@ import com.board.service.FileService;
 
 @Controller
 public class FileController {
+	@Resource(name = "fileService")
+	private FileService fileService;
+
+	@RequestMapping(value = "/file/download.do")
+	public ModelAndView downloadFile(HttpServletRequest request, HttpServletResponse response, Model model) {
+		int br_uid = Integer.parseInt(request.getParameter("br_uid"));
+		int f_uid = Integer.parseInt(request.getParameter("f_uid"));
+
+		fileService.downloadFile(f_uid, response);
+
+		System.out.println("====File Download Success ===");
+		ModelAndView mv = new ModelAndView("/view.do?uid=" + br_uid);
+
+		return mv;
+	}
+
+	@RequestMapping(value = "/file/ajax/upload.do", method = RequestMethod.POST)
+	public @ResponseBody FileVO ajaxUploadFile(MultipartHttpServletRequest multiRequest) {
+		System.out.println("FileController : ajaxUploadFile");
+		FileVO fileVO = new FileVO();
+		MultipartFile file = multiRequest.getFile("file");
+		if (file != null) {
+			fileVO = fileService.insertFileData(file);
+		}
+		System.out.println("return json :" + fileVO.toString());
+		return fileVO;
+	}
+
+	@RequestMapping(value = "/file/ajax/delete.do", method = RequestMethod.POST)
+	public @ResponseBody String ajaxDeleteFile(@RequestParam("uid") int uid) {
+		System.out.println("fileController : ajaxDeleteFile");
+		String resultString = "";
+
+		int result = fileService.deleteFileByUid(uid);
+
+		return resultString;
+
+	}
+	@RequestMapping(value = "/file/ajax/selectlist.do")
+	public @ResponseBody FileListVO ajaxSelectFiles(@RequestParam("uid") int uid) {
+		FileListVO result = new FileListVO();
+		List<FileVO> list = fileService.selectFilesByBoardUid(uid);
+		
+		System.out.println("GET FILE DATA OF BR_UID : " + uid + "...");
+		for(FileVO f : list) {
+			System.out.println(f.toString());
+		}
+		result.setSize(list.size());
+		result.setList(fileService.selectFilesByBoardUid(uid));
+		
+		return result;
+	}
 
 }
